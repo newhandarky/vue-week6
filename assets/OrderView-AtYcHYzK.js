4d06184f48337bab60857e47e99d52525f69e99e
newhandarky <newhandarky@gmail.com>
2024-03-09 22:43:20 +0800

    Updates

--- assets/OrderView-AtYcHYzK.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,a as t,b as r,F as n,e as s,o as a,r as c}from"./index-BpX9nGON.js";const _={},l=s("h2",null,"後訂單列表",-1);function d(i,m){const e=c("RouterView");return a(),t(n,null,[l,r(e)],64)}const f=o(_,[["render",d]]);export{f as default};
